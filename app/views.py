7cc0c68d3bd15f831a917fa44243617042a4145e
Sahadeb Rokaya <np03a190065@heraldcollege.edu.np>
2021-04-06 12:15:02 +0545

    login and customer registratiion

--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from .models import Image
 from .models import Contact
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+
+
 
 #from .forms import CustomerRegistrationForm
 
@@ -39,12 +44,38 @@ def photo(request):
     return render(request, 'app/photo.html')
 
 def customerregistration(request):
-    return  render(request,'app/customerregistration.html' )
+	if request.method =='POST':
+		print("hello")
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			form =UserCreationForm()
+	else:
+		form = UserCreationForm()	
+	return  render(request,'app/customerregistration.html', {'form':form})
     
 def loginn (request):
-    return render(request, 'app/login.html') 
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return HttpResponse("asjhf")
+		
+		# form = AuthenticationForm(request=request,user=request.POST)
+		# if form.is_valid():
+		# 	usr = form.cleaned_data['username']
+		# 	pwdd = form.cleaned_data['password']
+		# 	userss = authenticate(username=usr,password=pwdd)
+		# 	if userss is not None:
+		# 		login(request, userss)
+		# 		return HttpResponse("login success")
+
+	
+	form = AuthenticationForm()
+	return render(request, 'app/login.html',{'form':form}) 
 
 
-def Search (request):
-	render 
+
 	
