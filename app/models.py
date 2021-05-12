51749f57e49b87746cf1dc240cf853aa2c27c697
sahadeb1234 <63497241+sahadeb1234@users.noreply.github.com>
2021-05-12 17:54:43 +0545

    add models

--- app/models.py
+++ app/models.py
@@ -7,7 +7,9 @@ from django.core.validators import MaxLengthValidator
 class Image(models.Model):
     photo = models.ImageField(upload_to="myimage")
     title = models.CharField(max_length=50)
-    content = models.TextField(validators=[MaxLengthValidator(220)])
+    content = models.TextField()
+    # id = models.AutoField()
+    
     
     def __str__(self):
         return self.title 
@@ -17,21 +19,7 @@ class Contact(models.Model):
     email = models.EmailField()
     subject = models.TextField()
 
-class Booking(models.Model):
-    
-     travellingform = models.CharField(max_length=20)
-     travellingto =  models.CharField(max_length=20)
-     Departing = models.DateTimeField()
-     Returing = models.DateTimeField()
-     Adults = models.IntegerField()
-     Childern = models.IntegerField()
-    #  Travel_Types= (
-    #      ('Bus','Bus')
-    #      ('Aeo','Aeroplane')
-       
-
-    #  )
-     def __str__(self):
+    def __str__(self):
         return self.name
     
 
